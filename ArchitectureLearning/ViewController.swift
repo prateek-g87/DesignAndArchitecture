7bfb60c569ca336ff4df654dda7669eafca7b72b
Prateek Godhankar <prateek@Prateeks-MacBook-Pro.local>
2018-12-16 14:38:37 +0530

    Initial project added

---
+++ ArchitectureLearning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArchitectureLearning
+//
+//  Created by Prateek Godhankar on 16/12/18.
+//  Copyright © 2018 Prateek Godhankar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
